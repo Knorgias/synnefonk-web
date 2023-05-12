30ac40b6273c39452992ad5b4f71f8e2f60d6662
Konstantinos Norgias <Konstantinos.Norgias@ing.com>
2023-05-12 12:40:51 +0200

    feat: update topbar

--- packages/src/synnefonk-web-topbar/SynnefonkWebTopbar.js
+++ packages/src/synnefonk-web-topbar/SynnefonkWebTopbar.js
@@ -16,21 +16,33 @@ export class SynnefonkWebTopbar extends LitElement {
 
   static get styles() {
     return css`
-      .topbar {
-        width: 100%;
-        height: 200px;
+      :host {
+        display: block; /** Otherwise, it's displayed inline and not visible */
+        height: inherit;
         background-color: grey;
+      }
+
+      .topbar-container {
+        display: flex;
+        height: inherit;
+        justify-content: space-evenly;
+      }
+
+      .link-1:hover,
+      .link-2:hover,
+      .link-3:hover {
+        background-color: teal;
       }
     `;
   }
 
   render() {
-    html`
-      <div>
-        <a href=""></a><a href=""></a><a href=""></a><a href=""></a
-        ><a href=""></a><a href=""></a><a href=""></a><a href=""></a
-  
+    return html`<div class="topbar-container">
+      <div class="link-1">
+        <a href="../../../index.html">Homepage</a>
       </div>
-    `;
+      <div class="link-2"><a></a>Another Page</div>
+      <div class="link-3"><a></a>Github Repo</div>
+    </div>`;
   }
 }
