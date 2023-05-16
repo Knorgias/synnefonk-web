e831fa4c8c6c2acfc38adb6ad227ad8afa5c7817
Konstantinos Norgias <Konstantinos.Norgias@ing.com>
2023-05-16 17:57:51 +0200

    chore: introduce data structure for bookmark web component

---
+++ packages/_assets/pokemon/manifest.js
@@ -0,0 +1,110 @@
+export const manifestData = [
+  {
+    "filePath": "packages/_assets/pokemon/001",
+    "data": {
+      "number": "001",
+      "title": "Bulbasaur",
+      "description": "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+      "imagePath": "packages/_assets/pokemon/001/001.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/002",
+    "data": {
+      "number": "002",
+      "title": "Ivysaur",
+      "description": "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.",
+      "imagePath": "packages/_assets/pokemon/002/002.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/003",
+    "data": {
+      "number": "003",
+      "title": "Venusaur",
+      "description": "Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.",
+      "imagePath": "packages/_assets/pokemon/003/003.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/004",
+    "data": {
+      "number": "004",
+      "title": "Charmander",
+      "description": "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+      "imagePath": "packages/_assets/pokemon/004/004.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/005",
+    "data": {
+      "number": "005",
+      "title": "Charmeleon",
+      "description": "It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.",
+      "imagePath": "packages/_assets/pokemon/005/005.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/006",
+    "data": {
+      "number": "006",
+      "title": "Charizard",
+      "description": "It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.",
+      "imagePath": "packages/_assets/pokemon/006/006.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/007",
+    "data": {
+      "number": "007",
+      "title": "Squirtle",
+      "description": "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+      "imagePath": "packages/_assets/pokemon/007/007.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/008",
+    "data": {
+      "number": "008",
+      "title": "Wartortle",
+      "description": "It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.",
+      "imagePath": "packages/_assets/pokemon/008/008.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/009",
+    "data": {
+      "number": "009",
+      "title": "Blastoise",
+      "description": "It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.",
+      "imagePath": "packages/_assets/pokemon/009/009.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/010",
+    "data": {
+      "number": "010",
+      "title": "Caterpie",
+      "description": "For protection, it releases a horrible stench from the antenna on its head to drive away enemies.",
+      "imagePath": "packages/_assets/pokemon/010/010.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/011",
+    "data": {
+      "number": "011",
+      "title": "Metapod",
+      "description": "It is waiting for the moment to evolve. At this stage, it can only harden, so it remains motionless to avoid attack.",
+      "imagePath": "packages/_assets/pokemon/011/011.png"
+    }
+  },
+  {
+    "filePath": "packages/_assets/pokemon/012",
+    "data": {
+      "number": "012",
+      "title": "Butterfree",
+      "description": "In battle, it flaps its wings at great speed to release highly toxic dust into the air.",
+      "imagePath": "packages/_assets/pokemon/012/012.png"
+    }
+  }
+];
---
+++ packages/exports/bookmark.js
@@ -0,0 +1,3 @@
+import { SynnefonkBookmark } from '../src/synnefonk-bookmark/synnefonk-bookmark.js';
+
+customElements.define('synnefonk-bookmark', SynnefonkBookmark);
